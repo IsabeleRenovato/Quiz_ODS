cd11298d2f6972bf81bf130ef257e3af177a5ed3
Isabele Renovato <isabelerenovato@hotmail.com>
2023-04-08 20:28:12 -0300

    QuizODS

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    question: "Quantos Objetivos compõe os ODS?",
+    answers: [
+      { option: "10", correct: false },
+      { option: "13", correct: false },
+      { option: "17", correct: true },
+    ],
+  },
+  {
+    question: "Qual ODS fala sobre erradicar todas as formas de discriminação e empoderar mulheres e meninas?",
+    answers: [
+      { option: "Igualdade de Gênero", correct: true },
+      { option: "Saúde e Bem-Estar", correct: false },
+      { option: "Educação de qualidade", correct: false },
+    ],
+  },
+  {
+    question: "O que é Agenda 2030?",
+    answers: [
+      { option: "Plano de ação que reúne os objetivos para o desenvolvimento no milênio.", correct: false },
+      { option: "Plano de ação global que reúne os 17 Objetivos de desenvolvimento sustentável e suas metas.", correct: true },
+      { option: "Plano de ação que reúne os objetivos para o desenvolvimento no milênio.", correct: false },
+    ],
+  },
+  {
+    question: "Uma ação voluntária de reforço escolar e professores voluntários de um cursinho pré vestibular estão contribuindo com qual Objetivo?",
+    answers: [
+      { option: "Cidades e comunidades sustentáveis", correct: false },
+      { option: "Paz, amor e justiça", correct: false },
+      { option: "Educação e Qualidade", correct: true },
+    ],
+  },
+];
